82b1929c52b63b0ef5aac4c44dc09c015972650f
Kaleb Garrett <kalebagarrett@gmail.com>
2023-07-29 13:22:25 -0400

    Added more list of commands

--- JokeBot.App/Commands/HelpCommand.cs
+++ JokeBot.App/Commands/HelpCommand.cs
@@ -10,7 +10,12 @@ public class HelpCommand : CommandBase, ICommandHandler
     {
         await ReplyAsync($"**The following commands can be used for Joke Bot:**{Environment.NewLine}" +
                          $"  • **ping** - Pings the Discord channel{Environment.NewLine}" +
+                         $"  • **any** - Displays any kind of joke{Environment.NewLine}" +
                          $"  • **programming** - Displays a programming joke{Environment.NewLine}" +
+                         $"  • **dark** - Displays dark humor joke{Environment.NewLine}" +
+                         $"  • **pun** - Displays a pun{Environment.NewLine}" +
+                         $"  • **spooky** - Displays a spooky joke{Environment.NewLine}" +
+                         $"  • **christmas** - Displays a christmas joke{Environment.NewLine}" +
                          $"  • **misc** - Displays a miscellaneous joke{Environment.NewLine}");
     }
 }
